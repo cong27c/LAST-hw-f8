4b2dd07616fef90e143cfc84dec73558bcb589ba
Nguyễn Văn Công <nguyenvancongcbg27@hail.com>
2025-01-13 09:59:46 +0700

    bt-day28

---
+++ Day-28/ex01.js
@@ -0,0 +1,27 @@
+
+function updateCountdown() {
+    const newYear2026 = new Date("2026-01-01T00:00:00").getTime();
+    
+    const now = Date.now();
+    
+    const distance = newYear2026 - now;
+    console.log(distance);
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerText = days;
+    document.getElementById("hours").innerText = hours;
+    document.getElementById("minutes").innerText = minutes;
+    document.getElementById("seconds").innerText = seconds;
+
+    if (distance < 0) {
+        clearInterval(countdownInterval);
+        document.getElementById("countdown").innerText = "Chúc Mừng Năm Mới!";
+    }
+}
+
+const countdownInterval = setInterval(updateCountdown, 1000);
+
+updateCountdown();
